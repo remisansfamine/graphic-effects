d9e3bf50776663368d1d7243d20bd32aebff007d
r.giner <remi.giner@hotmail.fr>
2022-01-03 15:25:15 +0100

    First commit

---
+++ src/demo_base.h
@@ -0,0 +1,36 @@
+#pragma once
+
+#include <array>
+
+#include "demo.h"
+
+#include "opengl_headers.h"
+
+#include "camera.h"
+
+#include "tavern_scene.h"
+
+class demo_base : public demo
+{
+public:
+    demo_base(GL::cache& GLCache, GL::debug& GLDebug);
+    virtual ~demo_base();
+    virtual void Update(const platform_io& IO);
+
+    void RenderTavern(const mat4& ProjectionMatrix, const mat4& ViewMatrix, const mat4& ModelMatrix);
+    void DisplayDebugUI();
+
+private:
+    GL::debug& GLDebug;
+
+    // 3d camera
+    camera Camera = {};
+
+    // GL objects needed by this demo
+    GLuint Program = 0;
+    GLuint VAO = 0;
+
+    tavern_scene TavernScene;
+
+    bool Wireframe = false;
+};
---
+++ src/tavern_scene.h
@@ -0,0 +1,32 @@
+
+#include <vector>
+
+#include "opengl_helpers.h"
+
+// Tavern scene data (mapped on GPU)
+class tavern_scene
+{
+public:
+    tavern_scene(GL::cache& GLCache);
+    ~tavern_scene();
+    
+    // Mesh
+    GLuint MeshBuffer = 0;
+    int MeshVertexCount = 0;
+    vertex_descriptor MeshDesc;
+
+    // Lights buffer
+    GLuint LightsUniformBuffer = 0;
+    int LightCount = 8;
+
+    // Textures
+    GLuint DiffuseTexture = 0;
+    GLuint EmissiveTexture = 0;
+
+    // ImGui debug function to edit lights
+    void InspectLights();
+
+private:
+    // Lights data
+    std::vector<GL::light> Lights;
+};
